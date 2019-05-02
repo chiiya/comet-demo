89a1c5e83c55332024cb5aba7caf5a39570fac65
Elisha Witte <elishawitte@gmail.com>
2019-05-02 20:35:57 +0200

    Customize exception rendering

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Arr;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -47,5 +49,74 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert the given exception to an array.
+     *
+     * @param Exception $exception
+     *
+     * @return array
+     */
+    protected function convertExceptionToArray(Exception $exception): array
+    {
+        if (config('app.debug')) {
+            return [
+                'message' => $exception->getMessage(),
+                'exception' => \get_class($exception),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'trace' => collect($exception->getTrace())->map(function ($trace) {
+                    return Arr::except($trace, ['args']);
+                })->all(),
+            ];
+        }
+        $body = ['errors' => []];
+        $error = [];
+        $error['title'] = method_exists($exception, 'getTitle') ? $exception->getTitle() : 'Internal Server Error';
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() === 401) {
+                $error['title'] = 'Unauthorized';
+                $error['message'] = 'You are not authorized to access this resource.';
+            }
+            if ($exception->getStatusCode() === 404) {
+                $error['title'] = 'Record not found';
+                $error['message'] = 'The requested resource could not be found.';
+            } elseif ($exception->getStatusCode() === 405) {
+                $error['title'] = 'Method Not Allowed';
+                $error['message'] = 'The method specified in the request is not allowed for this resource.';
+            } else {
+                $error['message'] = $exception->getMessage();
+            }
+            $error['status'] = $exception->getStatusCode();
+        } else {
+            $error['message'] = 'Whoops, looks like something went wrong.';
+            $error['status'] = 500;
+        }
+        $body['errors'][] = $error;
+
+        return $body;
+    }
+
+    /**
+     * Convert a validation exception into a JSON response.
+     *
+     * @param \Illuminate\Http\Request                   $request
+     * @param \Illuminate\Validation\ValidationException $exception
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'errors' => [
+                [
+                    'title' => 'Invalid Request',
+                    'message' => $exception->getMessage(),
+                    'errors' => $exception->errors(),
+                    'status' => $exception->status,
+                ],
+            ],
+        ], $exception->status);
     }
 }
