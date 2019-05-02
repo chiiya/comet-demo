15d18c8f1470c6c9749b8f81ad70e5b9161b1234
Elisha Witte <elishawitte@gmail.com>
2019-05-02 20:36:22 +0200

    Fix wrong parameter used in route

--- routes/api/languages.php
+++ routes/api/languages.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('/languages', 'LanguageController@index')->name('languages.index');
-Route::get('/languages/{id}', 'LanguageController@show')->name('languages.show');
+Route::get('/languages/{code}', 'LanguageController@show')->name('languages.show');
 Route::post('/languages', 'LanguageController@store')->name('languages.store');
-Route::patch('/languages/{id}', 'LanguageController@update')->name('languages.update');
-Route::delete('/languages/{id}', 'LanguageController@delete')->name('languages.delete');
+Route::patch('/languages/{code}', 'LanguageController@update')->name('languages.update');
+Route::delete('/languages/{code}', 'LanguageController@delete')->name('languages.delete');
